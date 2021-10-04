464a14d0eb431a3452d16c3c2918006ef51369b8
Zachery Faria <zacheryfaria@gmail.com>
2021-10-03 21:01:50 -0500

    nmap

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+import nmap
+from cachetools import TTLCache, cached
+from threading import Thread
+import time
+
+nm = nmap.PortScanner()
 
 app = FastAPI()
 
+cache = {}
+
+
+def get_nmap_data():
+    results = nm.scan('192.168.1.*', arguments='-sn')
+    return {k: results['scan'][k]['hostnames'][0]['name'] for k in results['scan'].keys()}
+
+
+def update_nmap_results():
+    global cache
+    while True:
+        tmp = get_nmap_data()
+        time.sleep(5)
+        tmp.update(get_nmap_data())
+        cache = tmp
+        time.sleep(60)
+
+
+thread = Thread(target=update_nmap_results, daemon=True)
+
 
 @app.get("/")
-async def root(request: Request):
-    return {"message": "hello world", "root_path": request.scope.get("root_path")}
+def root():
+    return cache
 
+
+thread.start()
